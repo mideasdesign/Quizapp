0218f89ac8d149b7a8df397739c068a2311812e6
Markus Fischer <fischer@mideasdesign.de>
2025-03-28 11:26:34 +0100

    adding Questions and design the Card layout

---
+++ script.js
@@ -0,0 +1,82 @@
+{
+    "quiz": [
+      {
+        "question": "Welches ist das größte Tier der Welt?",
+        "answer": [
+          "Elefant",
+          "Blauwal",
+          "Giraffe",
+          "Nilpferd",
+          "Krokodil"
+        ],
+        "right_answer": "Blauwal"
+      },
+      {
+        "question": "Welcher Planet ist der Sonne am nächsten?",
+        "answer": [
+          "Venus",
+          "Mars",
+          "Merkur",
+          "Jupiter",
+          "Saturn"
+        ],
+        "right_answer": "Merkur"
+      },
+      {
+        "question": "Wie viele Kontinente gibt es auf der Erde?",
+        "answer": [
+          "5",
+          "6",
+          "7",
+          "8",
+          "9"
+        ],
+        "right_answer": "7"
+      },
+      {
+        "question": "Wer hat die Relativitätstheorie formuliert?",
+        "answer": [
+          "Isaac Newton",
+          "Albert Einstein",
+          "Galileo Galilei",
+          "Nikola Tesla",
+          "Stephen Hawking"
+        ],
+        "right_answer": "Albert Einstein"
+      },
+      {
+        "question": "Welches ist das höchste Gebirge der Welt?",
+        "answer": [
+          "Alpen",
+          "Rocky Mountains",
+          "Himalaya",
+          "Anden",
+          "Ural"
+        ],
+        "right_answer": "Himalaya"
+      },
+      {
+        "question": "Wie viele Beine hat eine Spinne?",
+        "answer": [
+          "4",
+          "6",
+          "8",
+          "10",
+          "12"
+        ],
+        "right_answer": "8"
+      },
+      {
+        "question": "Welche Farbe hat die Haut eines Eisbären unter dem Fell?",
+        "answer": [
+          "Weiß",
+          "Rosa",
+          "Schwarz",
+          "Grau",
+          "Gelblich"
+        ],
+        "right_answer": "Schwarz"
+      }
+    ]
+  }
+  
